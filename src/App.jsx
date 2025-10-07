39f0e1decabab350540654c3535764ebb1f67b7d
levired6 <levi.j.redinger@gmail.com>
2025-10-07 12:43:33 -0400

    Task 4.5 feature 1
    Filter events by city (Scenario 1)
    Given user hasn’t searched for any city;
    When the user opens the app;
    Then the user should see the list of upcoming events.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 
 // src/App.jsx
 import React from 'react';
+import { useEffect, useState } from 'react';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents'; // 1. Import the new component
+import { getEvents } from './api';
+
+
+import './App.css';
 
 const App = () => {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+
+   useEffect(() => {
+   fetchData();
+ }, []);
+
+ const fetchData = async () => {
+   const allEvents = await getEvents();
+   setEvents(allEvents.slice(0, currentNOE));
+ }
+
   return (
     <div className="App">
       <CitySearch />
       <NumberOfEvents /> {/* 2. Render the new component */}
-      <EventList />
+      <EventList events={events} />
     </div>
   );
 }
