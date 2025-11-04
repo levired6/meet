e533a30dddf7df787a06b6ca7a02acb8fc2dd288
levired6 <levi.j.redinger@gmail.com>
2025-11-04 16:01:48 -0500

    backing up
    Backing up version before cleaning out and updating

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import * as atatus from 'atatus-spa';
+atatus.config('5e915cd8496b411cb48d6410e1425258').install();
+
+atatus.notify(new Error('Test Atatus Setup'));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
