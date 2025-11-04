d2a5cc031b422aafeadc766c64cda09da6a45962
levired6 <levi.j.redinger@gmail.com>
2025-11-04 16:57:57 -0500

    Atatus set up and new vite/package-lock/node modules cleaned up
    Deleted and reinstalled node modules and package-lock.json because I had a bad older version of vite. Reinstalled and updated then atatus was able to connect to setup monitoring the application.

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,6 @@ import './index.css'
 import * as atatus from 'atatus-spa';
 atatus.config('5e915cd8496b411cb48d6410e1425258').install();
 
-atatus.notify(new Error('Test Atatus Setup'));
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
