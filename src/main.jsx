e47216026206f41becb0393eb70d0070eb1ed6ef
levired6 <levi.j.redinger@gmail.com>
2025-11-07 15:50:08 -0500

    service-worker.js & serviceWorkerRegistration.js
    Adding both service-worker.js & serviceWorkerRegistration.js files in src then registering them in main.jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import * as atatus from 'atatus-spa';
-atatus.config('5e915cd8496b411cb48d6410e1425258').install();
+//import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+
+//atatus.config('5e915cd8496b411cb48d6410e1425258').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
+serviceWorkerRegistration.register();
